6fa6436bdc7b28575029dabffbad2c619b4d9965
Tereza Vecková <te.veckova@gmail.com>
2021-05-17 15:39:23 +0200

    Přidány ikony menu

--- muj-projekt/src/index.js
+++ muj-projekt/src/index.js
@@ -1,3 +1,12 @@
 import './style.css';
 
 console.log('funguju!');
+
+const menu = document.querySelector('.nav-btn');
+
+menu.addEventListener('click', (event) => {
+  const navigace = document.querySelector('#hamburger');
+  navigace.classList.toggle('hamburger-opened');
+  const iconCross = document.querySelector('.nav-btn');
+  iconCross.classList.toggle('nav-btn--opened');
+});
