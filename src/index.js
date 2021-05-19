a42ff3447fb89e2da4952652d72450412a3181ba
Tereza Vecková <te.veckova@gmail.com>
2021-05-19 09:46:28 +0200

    Hlavička s menu

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ console.log('funguju!');
 const menu = document.querySelector('.nav-btn');
 
 menu.addEventListener('click', (event) => {
-  const navigace = document.querySelector('#hamburger');
-  navigace.classList.toggle('hamburger-opened');
+  const navigation = document.querySelector('#hamburger');
+  navigation.classList.toggle('hamburger-opened');
   const iconCross = document.querySelector('.nav-btn');
   iconCross.classList.toggle('nav-btn--opened');
 });
