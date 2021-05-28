908e7a9ef4c3c9ad90eebd1b81ed55405b0f2008
Tereza Sudíková <tereza.sudik@gmail.com>
2021-05-29 00:57:25 +0200

    stylovani homepage

--- src/index.js
+++ src/index.js
@@ -6,10 +6,12 @@ console.log('funguju!');
 const menu = document.querySelector('.nav-btn');
 
 menu.addEventListener('click', (event) => {
-  const navigation = document.querySelector('#hamburger');
-  navigation.classList.toggle('hamburger-opened');
-  const iconCross = document.querySelector('.nav-btn');
-  iconCross.classList.toggle('nav-btn--opened');
+  const open = document.querySelector('#hamburger');
+  open.classList.toggle('hamburger-opened');
+  const close = document.querySelector('.nav-btn');
+  close.classList.toggle('nav-btn--opened');
+  const topStripe = document.querySelector('#navigation');
+  topStripe.classList.toggle('stripe-opened');
 });
 
 const overviewElm = document.querySelector('#');
