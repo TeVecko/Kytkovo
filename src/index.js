da3ef2e1f11cf4deb1fb802b1954abe1bc83039b
Tereza Vecková <te.veckova@gmail.com>
2021-06-05 12:03:10 +0200

    smazani předchoziho commitu

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ const listPlant = {
   mint: {
     name: 'Máta klasnatá',
     latinName: 'Mentha spicata',
-    src: 'https://kytkovo.netlify.app/img/mint-table.jpg',
+    src: './img/mint-table.jpg',
     flowering: [june, july, august, september],
     href: 'Plants/mint.html/#view-plant',
   },
